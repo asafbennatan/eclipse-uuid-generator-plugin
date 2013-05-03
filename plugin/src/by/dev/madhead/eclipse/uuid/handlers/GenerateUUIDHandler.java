eb264b4a67ec5469653bffd9bf1c8260f0374737
madhead <siarhei.krukau@gmail.com>
2013-05-04 01:34:19 +0300

    Initial commit

---
+++ plugin/src/by/dev/madhead/eclipse/uuid/handlers/GenerateUUIDHandler.java
@@ -0,0 +1,56 @@
+package by.dev.madhead.eclipse.uuid.handlers;
+
+import java.util.UUID;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.MultiPageEditorPart;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public class GenerateUUIDHandler extends AbstractHandler {
+	public GenerateUUIDHandler() {
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		IWorkbenchPage page = window.getActivePage();
+		IEditorPart part = page.getActiveEditor();
+		ITextEditor editor = null;
+
+		if (part instanceof ITextEditor) {
+			editor = (ITextEditor) part;
+		} else if (part instanceof MultiPageEditorPart) {
+			MultiPageEditorPart multipageEditorPart = (MultiPageEditorPart) part;
+
+			if (multipageEditorPart.getSelectedPage() instanceof ITextEditor) {
+				editor = (ITextEditor) multipageEditorPart.getSelectedPage();
+			}
+		}
+
+		if (null == editor) {
+			return null;
+		}
+
+		IDocument doc = editor.getDocumentProvider().getDocument(
+				editor.getEditorInput());
+		ITextSelection sel = (ITextSelection) editor.getSelectionProvider()
+				.getSelection();
+
+		try {
+			doc.replace(sel.getOffset(), sel.getLength(), UUID.randomUUID()
+					.toString());
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
